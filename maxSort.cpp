5358dd673cbb0947ebdafe1c6d322e28ae77dfc5
benjaminraiford <benjaminhraiford@gmail.com>
2019-09-10 15:12:35 -0400

    Solved

--- maxSort.cpp
+++ maxSort.cpp
@@ -9,21 +9,11 @@ void print(const vector<int>& list);
 
 int main() {
 
-    vector<int> v(5);  // vector to be sorted
-    // initialize v
-    /* your code here */
+    vector<int> v {6, 1, 3, 2, 1};  // initialize v, the vector to be sorted
 
-
-    // print the unsorted v
-    /* your code here */
-
-
-    // sort v
-    /* your code here */
-
-
-    // print the sorted v
-    /* your code here */
+    print(v); // print the unsorted v
+    maxSort(v); // sort v
+    print(v);// print the sorted v
 
 
     return 0;
@@ -33,24 +23,29 @@ int main() {
 
 //Max sort function
 void maxSort(vector<int>& list){
-    for(int i=list.size()-1;i>=0;i--){
-        /*complete the code*/
-
-        for(int j=0;j<=i;j++){
-            /*complete the code*/
-
+    for(int i = list.size()-1; i>=0; i--){
+        int currentMax = 0;
+        for(int j=0; j <= i; j++){
+            if(list.at(j) > list.at(currentMax)){
+                currentMax = j;
+            }
         }
-        /*complete the code*/
+        swap(list.at(currentMax), list.at(i));
     }
 }
 
 // swap function
 void swap(int& a, int&b){
-    /*your code here*/
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 // print function
 void print(const vector<int>& list){
-    /*your code here*/
+    for (int i : list){
+        cout << i << " ";
+    }
+    cout << endl;
 }
 
